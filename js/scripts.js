0fb9227798e0b3e5334de70b93b57b58fca74f35
damunza <danidamunza@gmail.com>
2018-07-20 12:27:36 +0300

    added formula for rolling the dice

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,25 @@ $(document).ready(function(){
   $('.player1name').text(playerA);
   $('.player2name').text(playerB);
 })
+  $('button#newgame').click(function(){
+    $('#play-side').hide();
+    $('#registration').show();
+
+    //emptying the values
+    $('#dice-roll-1').empty();
+    $('#round-1').empty();
+    $('#total-1').empty();
+    $('#dice-roll-2').empty();
+    $('#round-2').empty();
+    $('#total-2').empty();
+  })
+  //formula for rolling the dice
+  var diceroll=function(){
+    return Math.floor(6*Math.random())+1;
+  }
+  //the first player rolling the dice
+  $('button#roll-1').click(function(){
+    throwingDice=diceroll();
+    $('dice-roll-1').text(throwingDice);
+  })
 })
