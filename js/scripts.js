9f36ec156150c5640c4d2a249e79a7e98e35aba4
damunza <danidamunza@gmail.com>
2018-07-20 12:55:40 +0300

    added the constructor and the prototypes for the players

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,35 @@ $(document).ready(function(){
   var diceroll=function(){
     return Math.floor(6*Math.random())+1;
   }
+  //creating a constructor for the players
+  function Player(turn){
+    this.roll=0;
+    this.rollvalue=0;
+    this.totalscore=0;
+    this.turn=turn;
+    this.name;
+  }
+  //if the player rolls a 1
+  Player.prototype.one=function(){
+    if(this.roll===1){
+      this.rollvalue=0;
+      return('your turn is over')
+    }else {
+      this.rollvalue +=this.roll;
+    }
+  }
+  //when the player presses hold
+  Player.prototype.hold=function(){
+    this.totalscore+=this.rollvalue;
+    this.rollvalue=0;
+    return('your turn is over');
+  }
+  //winning the game 
+  Player.prototype.winner=function(){
+    if (this.totalscore>=50) {
+      alert(this.name +'you won!!');
+    }
+  }
   //the first player rolling the dice
   $('button#roll-1').click(function(){
     throwingDice=diceroll();
